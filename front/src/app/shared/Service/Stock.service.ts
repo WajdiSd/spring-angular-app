e3cd60fc736d760391f25a0c9a7e8bbef2f6bc6c
geeekho <wajdi.sadouki@gmail.com>
2022-11-07 20:57:53 +0100

    changed api base_url

--- front/src/app/shared/Service/Stock.service.ts
+++ front/src/app/shared/Service/Stock.service.ts
@@ -7,7 +7,7 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class StockService {
-  readonly API_URL = 'http://localhost:8089/SpringMVC/stock';
+  readonly API_URL = 'http://192.168.31.203:8089/SpringMVC/stock';
 
   constructor(private httpClient: HttpClient) { }
 
